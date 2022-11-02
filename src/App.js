e4dac46dc1f5189dd89f1cbc393ccc87736a7303
jamescheng40 <cheng-james@hotmail.com>
2022-11-02 14:58:50 -0400

    [Previous Commits] Stuff related to Alert System and Portfolio control

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+
+//link for the tutorial below https://www.robinwieruch.de/react-checkbox/
+
+
 
 function App() {
+  const [checked, setChecked] = React.useState(false);
+  const [alarmforprice, setChecked1] = React.useState(false);
+  const [pricefallalarm, setChecked2] = React.useState(false);
+  const handleChange = () => {
+
+        setChecked(!checked);
+  };
+
+
+  const handleChangepricealarm = () => {
+
+    setChecked1(!alarmforprice);
+};
+
+const handlepricefallalarm = () => {
+
+  setChecked2(!pricefallalarm);
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Checkbox
+        label="ETH Or USDT"
+        value={checked}
+        onChange={handleChange}
+      />
+      <p>Is ETH? {checked.toString()}</p>
+
+      <Checkbox
+        label="Sound Alarm for bought price"
+        value={alarmforprice}
+        onChange={handleChangepricealarm}
+      />
+      <p>Sound alarm for reaching bought price? {alarmforprice.toString()}</p>
+      <br></br>
+
+      <Checkbox
+        label="Sound Alarm for drop price from recent high greater than 6 percent and below bought price of course"
+        value={pricefallalarm}
+        onChange={handlepricefallalarm}
+      />
+      <p>Sound Alarm for drop price from recent high greater than 6 percent? {pricefallalarm.toString()}</p>
+      
+      <label>
+      <Textfield
+        id="BoughtInUSD"
+      ></Textfield>  
+      Price bought or sold 
+      </label>
+
     </div>
   );
+};
+
+const Checkbox = ({ label, value, onChange }) => {
+  return (
+    <label>
+      <input type="checkbox" checked={value} onChange={onChange} />
+      {label}
+    </label>
+  );
 }
+
+const Textfield = ({id}) =>  
+{
+  return (
+    <label>
+      <input type="text" id={ "TF_" + id}/>
+    </label>
+  );
+
+}
+
+
+// const Button = ({id}) =>  
+// {
+//   return (
+//     <label>
+//       <input type="button" id={ "BTN_" + id}/>
+//     </label>
+//   );
+
+// }
 
 export default App;
