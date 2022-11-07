a96de0bd4cd2207b3bf5c98a35670cf427c07308
jamescheng40 <cheng-james@hotmail.com>
2022-11-07 01:17:45 -0500

    [Feat] Papa parse drop in file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
 import * as React from 'react';
-
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import Papa from 'papaparse';
 //link for the tutorial below https://www.robinwieruch.de/react-checkbox/
-
-
 
 function App() {
   const [checked, setChecked] = React.useState(false);
@@ -24,6 +23,35 @@ const handlepricefallalarm = () => {
 
   setChecked2(!pricefallalarm);
 };
+
+const changeHandler = (event) => {
+  Papa.parse(event.target.files[0], {
+    header: true,
+    skipEmptyLines: true,
+    complete: function (results) {
+      console.log(results.data)
+    },
+  });
+};
+
+const parseFile = file => {
+  Papa.parse(file);
+};
+
+const debug_handleimportbuttonclick = () => {
+
+  //seamingless import read csv data and display it onto the server
+  //csv file reader
+
+  Papa.parse(new File([Blob], "C:\\Users\\cheng\\Desktop\\james-study-file\\investingmentshit\\ethereumdatabase\\datafilevolclosefac1.02volratio3.0volperiod300.0timeperiod5mbelowline35.0wedgeblwline35.0rsiperiod30.0ATRperiod14.0STmultiplier2.0MA1period200.0MA2period50.0.csv"), 
+  {
+    complete: function(results) {
+      console.log("Finished:", results.data);
+    }
+
+  });
+};
+
 
   return (
     <div>
@@ -55,6 +83,33 @@ const handlepricefallalarm = () => {
       ></Textfield>  
       Price bought or sold 
       </label>
+
+      <br></br>
+
+    <label>
+      <Button id="debugbuttondata" value="test" onChange={debug_handleimportbuttonclick} >
+      </Button>
+      confirm to import default data
+    </label>
+
+    <div>
+      {/* File Uploader */}
+      <input
+        type="file"
+        name="file"
+        accept=".csv"
+        onChange={changeHandler}
+        style={{ display: "block", margin: "10px auto" }}
+      />
+    </div>
+
+      <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+      </LineChart>
 
     </div>
   );
