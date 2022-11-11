25d455fceaf88e99387a91f3902594396c1aa6c2
jamescheng40 <cheng-james@hotmail.com>
2022-11-10 19:47:36 -0500

    [Tool] parse and load local csv file for display on the frontend

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { getData2} from "./utils2.js"
 
 import {  TypeChooser } from "react-stockcharts/lib/helper";
 
+import { tsvParse, csvParse, csv, csvFormat, csvParseRows } from  "d3-dsv";
+
 import './main.css';
 
 var fakedata = 
@@ -23,15 +25,46 @@ var fakedata =
     "percentChange": ""
 }];
 
-const changeHandler = (event) => {
+const loaddata = (prop,event) => {
 
-	console.log("asdfasdf");
-	getData().then(data => {
-		this.setState({ data })
-	})
+	//filereader: https://www.w3docs.com/learn-javascript/file-and-filereader.html
+	let fileReader = new FileReader(); 
+	fileReader.readAsText(event.target.files[0]); 
+	fileReader.onload = function() {
+
+	//custom csv file parameter column from the csv producer in java; API doc:https://github.com/d3/d3-dsv:
+	const data = csvParseRows(fileReader.result, (d, i) => {
+		return {
+			date: +d[0], // convert first column to Date
+			close: +d[1],
+			rsiavggain: +d[2],
+			rsiavglose: +d[3], // convert fourth column to number
+			RSgainloseratio: +d[4],
+			RSIratio: +d[5],
+			volume: +d[6],
+			high: +d[7],
+			low: +d[8],
+			open: +d[9]
+			};
+		});
+		
+	  	console.log(data);
+		prop.setState({data});
+		
+	  }; 
+	  fileReader.onerror = function() {
+		alert(fileReader.error);
+	  }; 
+
 };
   
+const datachange = (prop) => {
 
+	console.log("asdsafasdfa2");
+
+	//getData2().then(data => {prop.setState({data})});
+
+};
 
 class ChartComponent extends React.Component {
 
@@ -53,7 +86,6 @@ class ChartComponent extends React.Component {
 			return <div>Loading...</div>
 		}
 		return (
-
 			<div>
 
 				<div class="navbar">
@@ -73,10 +105,11 @@ class ChartComponent extends React.Component {
 				<div>
 					{/* File Uploader */}
 					<input
+						id="fileinput"
 						type="file"
 						name="file"
 						accept=".csv"
-						onChange={() => getData2().then(data => {this.setState({data})})}
+						onChange={e => { loaddata(this,e); datachange(this) }}
 						style={{ display: "block", margin: "10px auto" }}
 					/>
 				</div>
