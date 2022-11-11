454331b38209c57e7a8f3b1ad8e997e403a7d06e
jamescheng40 <cheng-james@hotmail.com>
2022-11-11 03:29:28 -0500

    [Tool addition]Fix the bar chart width and added subplot with RSI indicator data from the csv

--- src/Chart.js
+++ src/Chart.js
@@ -10,6 +10,11 @@ import { XAxis, YAxis } from "react-stockcharts/lib/axes";
 import { fitWidth } from "react-stockcharts/lib/helper";
 import { last, timeIntervalBarWidth } from "react-stockcharts/lib/utils";
 
+import {RSISeries} from "react-stockcharts/lib/series";
+import {
+	MouseCoordinateY,} from "react-stockcharts/lib/coordinates";
+import { format } from "d3-format";
+
 class CandleStickChart extends React.Component {
 	render() {
 		const { type, width, data, ratio } = this.props;
@@ -21,7 +26,7 @@ class CandleStickChart extends React.Component {
 		return (
 			<div>
 			<ChartCanvas 
-					height={400}
+					height={600}
 					ratio={ratio}
 					width={width}
 					margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
@@ -32,12 +37,38 @@ class CandleStickChart extends React.Component {
 					xScale={scaleTime()}
 					xExtents={xExtents}>
 
-				<Chart id={1} yExtents={d => [d.high, d.low]}>
+				<Chart id={1} height={400} yExtents={d => [d.high, d.low]} padding={{ top: 10, bottom: 20 }} >
 					<XAxis axisAt="bottom" orient="bottom" ticks={6}/>
 					<YAxis axisAt="left" orient="left" ticks={5} />
-					<CandlestickSeries width={timeIntervalBarWidth(utcDay)}/>
+					{/* Todo:hard coded width make it depend on the time */}
+					<CandlestickSeries width={2}/>
 				</Chart>
+
+				<div></div>
+
+				{/* subplot example and RSI value from the csv file for different indicator for more example configuration checkout https://codesandbox.io/s/github/rrag/react-stockcharts-examples2/tree/master/examples/CandleStickChartWithRSIIndicator?file=/src/Chart.js */}
+				<Chart id={3}
+					yExtents={[0,100]}
+					height={125} origin={(w, h) => [0, h - 125]}
+				>
+					<XAxis axisAt="bottom" orient="bottom" showTicks={false} outerTickSize={0} />
+					<YAxis axisAt="right"
+						orient="right"
+						tickValues={[30, 50, 70]}/>
+					<MouseCoordinateY
+						at="right"
+						orient="right"
+						displayFormat={format(".2f")} />
+
+					<RSISeries yAccessor={d => d.RSIratio} />
+
+				</Chart>
+
+
 			</ChartCanvas>
+
+
+			
 			</div>
 		);
 	}
