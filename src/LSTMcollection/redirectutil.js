ae97cb44d46a2878092d9bc1f8d4f175ef6b61fd
jamescheng40 <cheng-james@hotmail.com>
2023-02-07 00:49:20 -0500

    [Core lib update] creating a page where we can refer to all the transfer link

---
+++ src/LSTMcollection/redirectutil.js
@@ -0,0 +1,8 @@
+export const Woderivationpage =  window.location.origin + "/LSTMderivation/dEwrtdWo"
+export const Wfderivationpage =  window.location.origin + "/LSTMderivation/dEwrtdWf"
+export const Widerivationpage =  window.location.origin + "/LSTMderivation/dEwrtdWi"
+export const Wctderivationpage = window.location.origin + "/LSTMderivation/dEwrtdWc"
+export const Wvderivationpage =  window.location.origin + "/LSTMderivation/dEwrtdWv"
+export const derivationmainpage =  window.location.origin + "/LSTMderivation"
+export const LSTMfullimple =  window.location.origin + "/LSTMimplementation"
+
