fa601c0829f3b34915ad7b2c49a2794cf2afadd8
jamescheng40 <cheng-james@hotmail.com>
2023-02-08 15:22:26 -0500

    [Core Content Update] About me content update

---
+++ src/LSTMcollection/Aboutmepage.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function Aboutme(){
+    return (
+        <div>
+            <p>Feel free to send me a Email if you have any question: <strong> cheng-james@hotmail.com</strong></p>
+            <p>Feel free to checkout github page: <strong>https://github.com/Jamescheng40</strong></p>
+            <p>Feel free to checkout my linkedin page <strong>https://ca.linkedin.com/in/james-cheng-5a5a08123</strong></p>
+        </div>
+    )
+}
+  
+export default Aboutme;
